47d843444290f548f414bbe9535bf178401dc2f2
walterswoodward <walter.sekope.woodward@gmail.com>
2021-05-14 11:16:18 -0700

    Whitelists alsome fields using the fillable property in Video model

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Video extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'director', 'rating', 'description', 'price', 'vulnerability'];
 }
