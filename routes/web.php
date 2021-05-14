777a064f5132074eeb363420b3b1bdbc6865f219
walterswoodward <walter.sekope.woodward@gmail.com>
2021-05-14 10:42:39 -0700

    Adds some example routes just to confirm everything is working

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+/**
+** Basic Routes for a RESTful service:
+**
+** Route::get($uri, $callback);
+** Route::post($uri, $callback);
+** Route::put($uri, $callback);
+** Route::delete($uri, $callback);
+**
+**/
+
+Route::get('videos', function () {
+    return response(['Video 1', 'Video 2', 'Video 3'],200);
+});
+
+Route::get('videos/{videoId}', function ($videoId) {
+    return response()->json(['videoId' => "{$videoId}"], 200);
+});
+
+
+Route::post('videos', function() {
+    return  response()->json([
+            'message' => 'Create success'
+        ], 201);
+});
+
+Route::put('videos/{video}', function() {
+    return  response()->json([
+            'message' => 'Update success'
+        ], 200);
+});
+
+Route::delete('videos/{video}',function() {
+    return  response()->json(null, 204);
 });
