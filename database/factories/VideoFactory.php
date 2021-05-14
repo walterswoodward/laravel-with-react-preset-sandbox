9f5b6c5debae7ecccbf7bf548dbd200fddc75c21
walterswoodward <walter.sekope.woodward@gmail.com>
2021-05-14 11:12:00 -0700

    Adds Video factory and sets DDatabaseSeeder.php to create 10

---
+++ database/factories/VideoFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Video;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class VideoFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Video::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->country(),
+            'director' => $this->faker->name(),
+            'rating' => $this->faker->numberBetween(0, 10),
+            'description' => $this->faker->text(),
+            'price' => $this->faker->numberBetween(5, 20),
+            'availability' => true
+        ];
+    }
+}
