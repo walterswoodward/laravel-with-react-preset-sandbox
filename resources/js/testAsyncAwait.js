6f9e3f4b9ede5e0650fb6d917c9081cd9e01b237
walterswoodward <walter.sekope.woodward@gmail.com>
2021-05-18 12:27:33 -0700

    Modifies code in testAsyncAwait.js to use Async Await rather than Promise then method

--- resources/js/testAsyncAwait.js
+++ resources/js/testAsyncAwait.js
@@ -4,17 +4,23 @@ function getVideos() {
     return axios.get('/videos');
 }
 
-function logVideos() {
-    let videos = getVideos();
-    if (getVideos() == undefined) {
-        console.log(videos[1]);
-    } else {
-        console.log('This does not work b/c axios.get returns a ' + videos + ' request is asynchronous and has not completed yet');
-        getVideos().then(response => {
-            console.log("Since axios.get returns a Promise, request data must be returned using the Promise.prototype.then() method:\n");
-            console.log(response.data[1]);
-        })
-    }
+async function logVideo(id) {
+    // let videos = getVideos();
+    // if (getVideos() == undefined) {
+    //     console.log(videos[1]);
+    // } else {
+    //     // Without Async Await:
+    //     console.log('This does not work b/c axios.get returns a ' + videos + ' request is asynchronous and has not completed yet');
+    //     getVideos().then(response => {
+    //         console.log("Since axios.get returns a Promise, request data must be returned using the Promise.prototype.then() method:\n");
+    //         console.log(response.data[id]);
+    //     })
+    //     // With Async Await:
+            let response = await getVideos();
+            console.log(response.data[id]);
+    // }
+
+
 }
 
-logVideos();
+logVideo(0);
