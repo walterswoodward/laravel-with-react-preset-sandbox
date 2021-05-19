d06a4471f4d77db06c8d23068bc1e753e93215e4
walterswoodward <walter.sekope.woodward@gmail.com>
2021-05-19 13:14:11 -0700

    Cleans up web.php a bit

--- routes/web.php
+++ routes/web.php
@@ -2,38 +2,46 @@
 
 use App\Http\Controllers\VideosController;
 use Illuminate\Support\Facades\Route;
-use Facades\App\Payment as Payment;
 use Illuminate\Support\Str;
 
-// Basic Implementation of a Laravel Facade
-// Route::get('/testfacade', function () {
-//     return Payment::process();
-// });
-
 // Real Time Facades Rebuilt as RealTimeFacade
-// spl_autoload_register(function ($name) {
-//     // die($name);
-//     if (Str::startsWith($name, 'RealTimeFacade')) {
-//         $stub = file_get_contents(app_path('facade.stub'));
+spl_autoload_register(function ($name) {
+    // die($name);
+    if (Str::startsWith($name, 'RealTimeFacade')) {
+        $stub = file_get_contents(app_path('facade.stub'));
 
-//         $namespace = str_replace('/', '\\', dirname(str_replace('\\', '/', $name)));
-//         $className = class_basename($name);
-//         $accessor = str_replace('RealTimeFacade', '', $namespace) . '\\' . $className;
+        $namespace = str_replace('/', '\\', dirname(str_replace('\\', '/', $name)));
+        $className = class_basename($name);
+        $accessor = str_replace('RealTimeFacade', '', $namespace) . '\\' . $className;
 
-//         $stub = str_replace(
-//             ['{namespace}', '{className}', '{accessor}'],
-//             [$namespace, $className, $accessor],
-//             $stub
-//         );
+        $stub = str_replace(
+            ['{namespace}', '{className}', '{accessor}'],
+            [$namespace, $className, $accessor],
+            $stub
+        );
 
-//         // Save newly created {className}Facade class to app/
-//         file_put_contents($path = app_path($className.'Facade.php'), $stub);
-//         require $path;
-//     }
-// });
+        // Save newly created {className}Facade class to app/
+        file_put_contents($path = app_path($className.'Facade.php'), $stub);
+        require $path;
+    }
+});
 
+// Example #1: Payment Class Called Directly
+Route::get('/testpaymentclass', function () {
+    return (new \App\Payment())->process();
+});
+
+// Example #2: Basic Implementation of Laravel Real Time Facades
+// see `storage/framework/cache/` for cached file
+Route::get('/testrealtimefacades', function () {
+    return Facades\App\Payment::process();
+});
+
+// Example #3: Uses the same underlying Payment class,
+// but with the RealTimeFacade created in the call to
+// spl_autoload_register above
 Route::get('/testrealtimefacade', function () {
-    return Payment::process();
+    return RealTimeFacade\App\Payment::process();
 });
 
 Route::get('/', function () {
