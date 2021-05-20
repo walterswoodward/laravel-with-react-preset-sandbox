74e03abcc027b79ae4bde2561567257afd597be4
walterswoodward <walter.sekope.woodward@gmail.com>
2021-05-20 15:32:06 -0700

    Sets primary key of post_tag table to a composite id to avoid duplicate pairs of post + tag - Adds code to delete any records in post_tag if either the associated post or tag are deleted

--- database/migrations/2021_05_20_213651_create_post_tag_table.php
+++ database/migrations/2021_05_20_213651_create_post_tag_table.php
@@ -14,9 +14,9 @@ class CreatePostTagTable extends Migration
     public function up()
     {
         Schema::create('post_tag', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('post_id')->constrained();
-            $table->foreignId('tag_id')->constrained();
+            $table->primary(['post_id', 'tag_id']);
+            $table->foreignId('post_id')->constrained()->onDelete('cascade');
+            $table->foreignId('tag_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
