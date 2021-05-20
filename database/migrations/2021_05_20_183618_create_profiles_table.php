760a5176fc36b1b69a3f081030e18bed5b2008bc
walterswoodward <walter.sekope.woodward@gmail.com>
2021-05-20 12:00:48 -0700

    Create Profile table/model aand one to one relation with User - Adds one to one relationship from users table to profiles table with profile function and hasOne method - Creates Profile model and migration - Updates Profile migration file to include three url type fields and foreign key (user_id) to corresponding user - Adds one to one relationship from profiles table to users table with the user function and hasOne method - Adds code to create 10 new profiles using Database Seeder

---
+++ database/migrations/2021_05_20_183618_create_profiles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('user_id');
+            $table->string('website_url');
+            $table->string('github_url');
+            $table->string('twitter_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Profile;
 use App\Models\User;
 use App\Models\Video;
 use Illuminate\Database\Seeder;
@@ -17,5 +18,6 @@ class DatabaseSeeder extends Seeder
     {
         User::factory(10)->create();
         Video::factory(10)->create();
+        Profile::factory(10)->create();
     }
 }
